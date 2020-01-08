df8e46cb6cf9f5cfd6b0cb744a9975b8f3a6aaf1
Josh Bryson <josh@bryson.me.uk>
2020-01-08 17:28:23 +0000

    Simple SASS styling and Header component created

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
+import { Header } from '../components/Header/Header';
+import './App.scss';
 
 interface AppProps {
     todos: Todo[];
@@ -40,27 +42,41 @@ class _App extends Component<AppProps, AppState> {
     renderList(): JSX.Element[] {
         return this.props.todos.map((todo: Todo) => {
             return (
-                <div 
-                    key={todo.id}
-                    onClick={() => this.onTodoClick(todo.id)}
-                >
-                    {todo.title}
+                <div className="list__container">
+                    <div className="list__item"
+                        key={todo.id}
+                        onClick={() => this.onTodoClick(todo.id)}
+                    >
+                        {todo.title}
+                    </div>
                 </div>
             )
         })
     }
 
+    loadingMessage():JSX.Element {
+        return (
+            <div className="list__loading">
+                <h4>Loading...</h4>
+            </div>
+        )
+    }
+
     render(){
         return (
-            <div>
-                <button
-                    onClick={this.onButtonClick}
-                >
-                    Fetch List
-                </button>
-                {this.state.fetching ? 'LOADING...' : null}
-                {this.renderList()}
-            </div>
+            <Fragment>
+                <Header />
+                <div className="list">
+                    <button
+                        className="list__button"
+                        onClick={this.onButtonClick}
+                    >
+                        Fetch List
+                    </button>
+                    {this.state.fetching ? this.loadingMessage() : null}
+                    {this.renderList()}
+                </div>
+            </Fragment>   
         )
     }
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from "react";
+import "./Header.scss";
+
+export function Header() {
+
+  return (
+    <header className="Header">
+      <h3 className="navbar-heading">
+            React Redux TypeScript Protoype
+        </h3>
+    </header>
+  );
+}
